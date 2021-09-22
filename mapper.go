e06ea97777a1710544e0d2dd1ddba020e75dc5b5
yugovtr <vtrhg69@hotmail.com>
2021-09-22 00:07:51 -0300

    :sparkles: (feature) map pointer fields

--- mapper.go
+++ mapper.go
@@ -6,18 +6,28 @@ import (
 )
 
 var (
-	errStruct = errors.New("need struct")
+	errStruct = errors.New("need to be structs")
+	errPtr    = errors.New("need to be pointer")
 )
 
-func Mapper(source interface{}, target interface{}) error {
-	rv := reflect.ValueOf(target).Elem()
-	rvs := reflect.ValueOf(source)
+func Mapper(source interface{}, target interface{}) (err error) {
+	t := reflect.ValueOf(target)
+	if t.Kind() != reflect.Ptr {
+		return errPtr
+	}
 
+	rv := t.Elem()
 	if rv.Kind() != reflect.Struct {
 		return errStruct
 	}
 
+	rvs := reflect.ValueOf(source)
+	if rvs.Kind() != reflect.Struct {
+		return errStruct
+	}
+
 	for i := 0; i < rvs.NumField(); i++ {
+		f := rvs.Field(i)
 		vn := rvs.Type().Field(i).Name
 		vt := rvs.Type().Field(i).Type.Kind()
 
@@ -27,15 +37,19 @@ func Mapper(source interface{}, target interface{}) error {
 			continue
 		}
 
-		if s.Kind() == reflect.Struct {
-			Mapper(rvs.Field(i).Interface(), s.Addr().Interface())
-			continue
-		}
-
-		if s.CanSet() {
-			s.Set(rvs.Field(i))
+		switch s.Kind() {
+		case reflect.Ptr:
+			if f.Elem().Kind() == reflect.Struct {
+				err = Mapper(f.Elem().Interface(), s.Interface())
+			}
+		case reflect.Struct:
+			err = Mapper(f.Interface(), s.Addr().Interface())
+		case vt:
+			if s.CanSet() {
+				s.Set(f)
+			}
 		}
 	}
 
-	return nil
+	return err
 }
--- mapper_test.go
+++ mapper_test.go
@@ -1,6 +1,7 @@
 package mapper
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,49 +9,68 @@ import (
 
 func TestMapper(t *testing.T) {
 
-	type subSource struct {
-		D string
-	}
-
-	type subTarget struct {
-		D string
-	}
+	type subSource struct{ D string }
+	type subTarget struct{ D string }
 
 	type source struct {
-		A string
-		B int
-		C bool
-		S subSource
+		A  string
+		B  int
+		C  bool
+		E  []float32
+		S  subSource
+		P  *subSource
+		PP string
 	}
 
 	type target struct {
-		A string
-		B string
-		S subTarget
+		A  string
+		B  string
+		E  []float32
+		S  subTarget
+		P  *subTarget
+		PP *subTarget
 	}
 
 	tests := map[string]struct {
-		target     target
+		target     *target
 		source     source
-		wantTarget target
+		wantTarget *target
 		wantErr    bool
 	}{
 		"when target has the source fields": {
-			target:     target{},
-			source:     source{A: "A"},
-			wantTarget: target{A: "A"},
+			target:     &target{},
+			source:     source{A: "A", E: []float32{1.0, 2.0}},
+			wantTarget: &target{A: "A", E: []float32{1.0, 2.0}},
 			wantErr:    false,
 		},
 		"when source has sub-fields": {
-			target:     target{},
+			target:     &target{},
 			source:     source{A: "A", S: subSource{D: "D"}},
-			wantTarget: target{A: "A", S: subTarget{D: "D"}},
+			wantTarget: &target{A: "A", S: subTarget{D: "D"}},
 			wantErr:    false,
 		},
 		"when has fields with same name and different types": {
-			target:     target{},
+			target:     &target{},
 			source:     source{A: "A", B: 0},
-			wantTarget: target{A: "A", B: ""},
+			wantTarget: &target{A: "A", B: ""},
+			wantErr:    false,
+		},
+		"when target is nil": {
+			target:     nil,
+			source:     source{A: "A"},
+			wantTarget: nil,
+			wantErr:    true,
+		},
+		"when has sub-field pointer": {
+			target:     &target{P: &subTarget{D: ""}},
+			source:     source{P: &subSource{D: "D"}},
+			wantTarget: &target{P: &subTarget{D: "D"}},
+			wantErr:    false,
+		},
+		"when has pointer with same name and different types": {
+			target:     &target{PP: &subTarget{D: ""}},
+			source:     source{PP: ""},
+			wantTarget: &target{PP: &subTarget{D: ""}},
 			wantErr:    false,
 		},
 	}
@@ -59,9 +79,27 @@ func TestMapper(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			assertion := assert.New(t)
 
-			err := Mapper(tt.source, &tt.target)
+			err := Mapper(tt.source, tt.target)
 			assertion.Equal((err != nil), tt.wantErr, "Mapper() error: expected %v, got %v", tt.wantErr, err)
 			assertion.Equal(tt.target, tt.wantTarget, "Mapper(): expected %#v, got %#v", tt.wantTarget, tt.target)
 		})
 	}
+}
+
+func ExampleMapper() {
+	target := struct {
+		A string
+		B int
+		C bool
+	}{}
+	source := struct {
+		A string
+		B int
+		C bool
+	}{A: "A", B: 1, C: true}
+	err := Mapper(source, &target)
+	fmt.Printf("Target: %v\nError: %v", target, err)
+	//Output:
+	//Target: {A 1 true}
+	//Error: <nil>
 }
